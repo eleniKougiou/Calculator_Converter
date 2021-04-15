9490fdc85ad922f1ab3dc208ff070476e29c8181
eleniKougiou <elkoum15@gmail.com>
2021-04-15 03:01:30 +0300

    Create Json.java

---
+++ app/src/main/java/com/test/simplecalculator/Json.java
@@ -0,0 +1,87 @@
+package com.test.simplecalculator;
+
+import androidx.annotation.NonNull;
+
+//import com.google.gson.annotations.Expose;
+//import com.google.gson.annotations.SerializedName;
+
+import java.util.HashMap;
+
+public class Json {
+
+    //@SerializedName("success")
+    //@Expose
+    private boolean success;
+    //@SerializedName("timestamp")
+    //@Expose
+    private int timestamp;
+    //@SerializedName("base")
+    //@Expose
+    private String base;
+    //@SerializedName("date")
+    //@Expose
+    private String date;
+    //@SerializedName("rates")
+    //@Expose
+    public HashMap<String, Double> rates;
+
+    public Json(boolean success, int timestamp, String base, String date, HashMap<String, Double> rates) {
+        this.success = success;
+        this.timestamp = timestamp;
+        this.base = base;
+        this.date = date;
+        this.rates = rates;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public int getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(int timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getBase() {
+        return base;
+    }
+
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public HashMap<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(HashMap<String, Double> rates) {
+        this.rates = rates;
+    }
+
+    @Override
+    //@NonNull
+    public String toString() {
+        return "ApiResponse{" +
+                "success=" + success +
+                ", timestamp=" + timestamp +
+                ", base='" + base + '\'' +
+                ", date='" + date + '\'' +
+                ", rates=" + rates +
+                '}';
+    }
+}
